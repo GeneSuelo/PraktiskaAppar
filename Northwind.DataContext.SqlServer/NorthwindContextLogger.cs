f83494894a304ed0e2b8b47eb65659745adfcb2b
Gene Suelo <gene.suelo@gmail.com>
2025-03-22 11:37:56 +0100

    Logic metoden i NortheinContextLogger

--- Northwind.DataContext.SqlServer/NorthwindContextLogger.cs
+++ Northwind.DataContext.SqlServer/NorthwindContextLogger.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace Northwind.DataContext.SqlServer
 {
     /// <summary>
     /// Logger for NorthwindContext, ska logga till fil
     /// </summary>
-    internal class NorthwindContextLogger
+    public class NorthwindContextLogger
     {
+       public static void WriteLine(string message)
+        {
+            string path = Path.Combine(GetFolderPath(SpecialFolder.DesktopDirectory), "northwindlog.txt");
+
+            StreamWriter textFile = File.AppendText(path);
+            textFile.WriteLine(message);
+            textFile.Close();
+        }
+       
     }
 }
