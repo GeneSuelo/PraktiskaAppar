7999903344a415779a923a906c5929276c094f2c
Gene Suelo <gene.suelo@gmail.com>
2025-03-26 17:00:39 +0100

    Post Suppliers Orders

--- Northwind.Web2/Pages/Suppliers.cshtml.cs
+++ Northwind.Web2/Pages/Suppliers.cshtml.cs
@@ -12,12 +12,30 @@ namespace Northwind.Web.Pages
             this.db = injectedContext;
         }
         public IEnumerable<Supplier>? Suppliers { get; set; }
+
+        [BindProperty]
+        public Supplier? Supplier { get; set; }
         public void OnGet()
         {
             ViewData["Title"] = "Northwind App - Suppliers";
 
             Suppliers = db.Suppliers.OrderBy(c => c.Country)
                 .ThenBy(c => c.CompanyName);
+        }
+
+        public IActionResult OnPost() 
+        {
+            if((Supplier is not null) && ModelState.IsValid) 
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            else 
+            {
+                return Page(); //returnerar ursprungliga sidan
+            }
+            
         }
     }
 }
