0dee7c0f7e6bdb4767c1766a71821973f35387e1
Gene Suelo <gene.suelo@gmail.com>
2025-03-26 18:23:17 +0100

    Partial view - Employee

--- Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
+++ Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Northwind.EntityModels;
 
 namespace Northwind.Razor.Employees.MyFeature.Pages
 {
-    public class EmployeesMode : PageModel
+    public class EmployeesModel : PageModel
     {
+        private NorthwindDatabaseContext db;
+
+        public EmployeesModel(NorthwindDatabaseContext injectedContext) 
+        {
+            db = injectedContext;
+        }
+
+        public Employee[] Employees { get; set; } = null!;
         public void OnGet()
         {
             ViewData["Title"] = "Northwind App - Employees";
             //TODO: Implementera kod för att hämta
             //employees från databased, sortera med LastName sen FrstName
+            Employees = db.Employees.OrderBy(e => e.LastName)
+                .ThenBy(e => e.FirstName).ToArray();
         }
     }
 }
+ 
