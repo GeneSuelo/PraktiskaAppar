20b59a0706ad7a1add6c32e3cfbe833f91ef3ba8
Gene Suelo <gene.suelo@gmail.com>
2025-03-26 17:42:11 +0100

    Razor klass bibliotek for employees

---
+++ Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Razor.Employees.MyFeature.Pages
+{
+    public class EmployeesMode : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind App - Employees";
+            //TODO: Implementera kod för att hämta
+            //employees från databased, sortera med LastName sen FrstName
+        }
+    }
+}
